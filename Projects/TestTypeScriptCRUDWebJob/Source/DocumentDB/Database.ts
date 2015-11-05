6039e4ab8b952e82f628580ca16ac846d0370a4d
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2015-11-05 10:49:33 -0300

    update

---
+++ Projects/TestTypeScriptCRUDWebJob/Source/DocumentDB/Database.ts
@@ -0,0 +1,12 @@
+/// <reference path="../../typings/documentdb/documentdb.d.ts" />
+// import account = require("../DocumentDB/Collection");
+import DocumentDB = require('documentdb');
+
+export class Database {
+  private _listCollection;
+  private _id:string;
+  
+  public constructor(){
+    
+  }
+}
---
+++ Projects/TestTypeScriptCRUDWebJob/Source/DocumentDB/DocumentDB.ts
@@ -0,0 +1,32 @@
+/// <reference path="../../typings/documentdb/documentdb.d.ts" />
+ import account = require("../DocumentDB/Database");
+import documentDB = require('documentdb');
+var env = require('dotenv').load();
+export class DocumentDB {
+  private _listDatabase;
+  private _endPoint:string;
+  private _authKey:string;
+  private _client:documentDB.DocumentClient;
+  
+  public constructor(endPoint?:string, authKey?:string){
+    this._endPoint = name|| process.env.AZURE_DOCUMENTDB_ENDPOINT;
+    this._authKey = name||process.env.AZURE_DOCUMENTDB_AUTH_KEY;
+    this._client = new documentDB.DocumentClient(this._endPoint, {masterKey: this._authKey});
+  }
+  
+  public get endPoint():string{
+        return this._endPoint;
+  }
+  
+  public get authKey():string{
+        return this._authKey;
+  }
+  
+  public get client():documentDB.DocumentClient {
+        return this.client;
+  }
+  
+  public get listDatabase() {
+        return this._listDatabase;
+  }
+}
